c49c416f6ec33bbcb47639fc1d2687140cc5454e
Dee Dee Raye McCroskey <deedee.mccroskey@gmail.com>
2017-11-19 15:59:08 -0600

    Add files via upload
    Tester file for project.

---
+++ Tester.java
@@ -0,0 +1,60 @@
+package tests;
+import problemDomain.DiceBag;
+import problemDomain.NumberOfDiceException;
+import problemDomain.NumberOfFacesException;
+import problemDomain.Dice;
+
+import java.util.*;
+
+/**
+ * This class tests the DiceBag and Dice classes.
+ * @author Dee Dee McCroskey
+ * @since 09/19/16 
+ */
+public class Tester {
+	
+//	public static void main(String args[]) throws NumberOfFacesException, NumberOfDiceException
+//	{
+//		int numberOfDice;	
+//		int numberOfFaces;	
+//		Scanner keyboardReader=new Scanner(System.in);
+//		
+//		//introduce test
+//		System.out.println("Testing DiceBag\n");
+//		
+//		//collect information from user
+//		System.out.print("Dice Wanted: ");
+//		numberOfDice=keyboardReader.nextInt();
+//		System.out.print("Number of Faces Wanted: ");
+//		numberOfFaces=keyboardReader.nextInt();
+//		
+//		//create the DiceBag
+//		DiceBag baggieOfDice=new DiceBag(numberOfDice, numberOfFaces);
+//		
+//		//roll the dice in the bag
+//		baggieOfDice.rollAllDice();
+//		
+//		//print out values of each face roll 
+//		System.out.println("Faces: " + baggieOfDice.toString());
+//		System.out.println("Total: " + baggieOfDice.getTotalRoll());
+//		
+//		System.out.println("\nTest Frequency of Dice\n");
+//		
+//		//collect information about Dice
+//		System.out.print("Number of Faces: ");
+//		numberOfFaces=keyboardReader.nextInt();
+//		
+//		//create the Dice
+//		Dice die=new Dice(numberOfFaces);
+//
+//		  
+//		
+//		//test frequency
+//		for(int x = 1; x<=100; x++)
+//		{
+//			System.out.println("Trail " +x +": " + die.roll());
+//		}
+//		
+//	}
+
+}
